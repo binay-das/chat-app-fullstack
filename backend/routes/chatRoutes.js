b385b6bed9fec90ad39d05c1b3c88dab29a87c7d
Binay <binaydas1123@gmail.com>
2024-11-25 10:21:50 +0530

    Chat routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middlewares/authMiddleware');
+const { accessChats, fetchChats, createGroupChat } = require('../controllers/chatControllers');
 
-// router.route('/').post(protect, accessChats);
-// router.route('/').get(protect, fetchChats);
-// router.route('/group').post(protect, createGroupChat);
+router.route('/').post(protect, accessChats);
+router.route('/').get(protect, fetchChats);
+router.route('/group').post(protect, createGroupChat);
 // router.route('/rename').put(protect, renameGroup);
 // router.route('/remove').put(protect, removeFromGroup);
 // router.route('/goupadd').put(protect, addToGroup);
