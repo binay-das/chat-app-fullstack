578a38ed0a23f3b37285cc2415eb130990a86254
Binay <binaydas1123@gmail.com>
2024-11-25 10:07:02 +0530

    Error middlewares

--- backend/middlewares/errorMiddlewares.js
+++ backend/middlewares/errorMiddlewares.js
@@ -12,7 +12,7 @@ const errorHandler = (err, req, res, next) => {
     res.status(statusCode);
     res.json({
         message: err.message,
-        statck: process.env.NODE_ENV === 'production' ? null : err.statck,
+        stack: process.env.NODE_ENV === 'production' ? null : err.statck,
     });
 }
 
